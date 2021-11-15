6792166708adad3736bb82d0894a86582e4e4b9c
Matupoom Chaiwan <matupoom001@gmail.com>
2021-11-15 15:27:53 +0700

    api add story

--- src/controller/StoryController.ts
+++ src/controller/StoryController.ts
@@ -1,5 +1,6 @@
 import { Router, Response, Request } from "express"
 import StoryModel from "../models/Story/StoryModel";
+import StoryRequestModel from "../models/Story/StoryRequestModel";
 import { StoryService } from "../services/StoryService";
 
 export class StoryController {
@@ -14,11 +15,17 @@ export class StoryController {
 
   public routes(){
     this.router.get('/', this.getAll)
+    this.router.post('/add', this.addStory)
   }
   
   public getAll = async (req: Request, res: Response<Array<StoryModel>>) => {
     const storys = await this.storyService.getStory()
     res.send(storys).json()
-  } 
+  }
+
+  public addStory = async (req: Request<{}, {}, StoryRequestModel>, res: Response<StoryModel>) => {
+    const storys = await this.storyService.addStory(req.body)
+    res.send(storys).json()
+  }
   
 }
--- src/services/StoryService.ts
+++ src/services/StoryService.ts
@@ -2,6 +2,7 @@ import { getConnection } from 'typeorm';
 import { StoryEntity } from '../database/entities/StoryEntity';
 import { UserEntity } from '../database/entities/UserEntity';
 import StoryModel from '../models/Story/StoryModel';
+import StoryRequestModel from '../models/Story/StoryRequestModel';
 import UserModel from '../models/UserModel';
 import { StoryRepository } from '../repository/StoryRepository';
 import { UserRepository } from '../repository/UserRepository';
@@ -34,6 +35,26 @@ export class StoryService {
       console.error(error)
     }
     return []
+  }
+
+  public async addStory(request: StoryRequestModel): Promise<StoryModel> {
+    try {
+      const newStory: StoryEntity = new StoryEntity
+      newStory.contents = request.contents
+      newStory.picture = request.picture
+      newStory.owner = await this.userRepository.findOne({where: {id: request.ownerId}, relations: ['role', 'type']}) as UserEntity
+      const story: StoryEntity = await this.storyRepository.save(newStory)
+      let result: StoryModel = {
+        storyId: story.id,
+        contents: story.contents,
+        picture: story.picture,
+        owner: this.mapUserEntityToUserModel(story.owner)
+      }
+      return result
+    } catch (error) {
+      console.error(error)
+    }
+    return new StoryModel
   }
 
   private mapUserEntityToUserModel(userEntity: UserEntity): UserModel {
